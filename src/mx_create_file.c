6974dc0da112b12ef140a855c5ac61cc3b977b7f
okhomin <okhomin@e2r5p2.unit.ua>
2019-12-06 18:02:29 +0200

    fixed bug with get dir

--- src/mx_create_file.c
+++ src/mx_create_file.c
@@ -1,7 +1,7 @@
 #include "uls.h"
 
 t_file* mx_create_file(char *relative_path, char *filename) {
-    t_file *new_file = (t_file*)malloc(sizeof(t_file));
+    t_file *new_file = (t_file*) malloc(sizeof(t_file));
     struct stat file_stat;
 
     new_file->filename = mx_strdup(filename);
--- src/mx_new_file.c
+++ src/mx_new_file.c
@@ -4,5 +4,6 @@ t_files *mx_new_file(t_file *file) {
     t_files *files = malloc(sizeof(t_files));
 
     files->file = file;
+    files->next = NULL;
     return files;
 }
