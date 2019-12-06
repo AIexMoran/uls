42ed9287690a4d255252fbd11b9efee073d35cd3
okhomin <okhomin@e2r5p2.unit.ua>
2019-12-06 19:30:37 +0200

    edited mx_uls

--- src/mx_uls.c
+++ src/mx_uls.c
@@ -3,8 +3,10 @@
 #define FLAGS "l"
 
 int main(int argc, char **argv) {
+    int end_flags = mx_get_end_flags(argv, argc);
+    
     mx_check_flags(argv, FLAGS, argc);
-    mx_sort_args(argc, mx_get_end_flags(argv, argc), argv); // ready
+    mx_sort_args(argc, end_flags, argv); // ready
 
     t_file *file = mx_create_file("", "src/");
     t_files *files = mx_get_files_dir(file);
