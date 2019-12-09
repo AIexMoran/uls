3774e826e70881e7dc8af65d65e8a20d86e383fc
okhomin <okhomin@e2r5p2.unit.ua>
2019-12-09 19:00:04 +0200

    edited files

--- src/mx_handle_files.c
+++ src/mx_handle_files.c
@@ -3,14 +3,14 @@
 static void del_all(t_files *files, t_files *dirs, t_files *all_files);
 
 void mx_handle_files(char **argv, int size, int flags) {
-    t_files *files = mx_get_files_arg(argv, size);
-    t_files *dirs = mx_get_dirs_arg(argv, size);
-    t_files *all_files = mx_get_all_arg(argv, size);
+    t_files *files = mx_get_files_arg(argv, size); // get files from argv
+    t_files *dirs = mx_get_dirs_arg(argv, size); // get dirs from argv
+    t_files *all_files = mx_get_all_arg(argv, size); // get all from argv
 
-    mx_set_total(dirs, flags);
+    mx_set_total(dirs, flags); // set total to dirs
     //test
     if (MX_F_ISDL(flags)) { // if flag 'd' print all files and dirs without contents
-        mx_print_files(all_files, flags);
+        mx_print_files(all_files, flags); // print all files
     }
     else { // else prints files and contents of dir
         if (files)
--- src/mx_print_files.c
+++ src/mx_print_files.c
@@ -4,7 +4,7 @@ static void print_error(char *filename);
 
 void mx_print_files(t_files *files, int flags) { //TODO
     for (t_files *cur = files; cur; cur = cur->next) {
-        if (cur->file->den_perms) {
+        if (cur->file->den_perms) { // checks if file has perms denied
             print_error(cur->file->filename);
             continue;
         }
