4b4c6bd93015aac3cf74a1573bb1aaa1655d44c6
mavramenko <mavramenko@okhomins-iMac.local>
2020-01-07 16:49:13 +0200

    reverse_files added

--- src/mx_sort_files_flags.c
+++ src/mx_sort_files_flags.c
@@ -1,5 +1,7 @@
 #include "uls.h"
 
+t_files *reverse_files(t_files *files, int flags);
+
 void mx_sort_files_flags(t_files *files, int flags) {
     if (MX_F_ISFL(flags)) {
         return;
@@ -9,4 +11,23 @@ void mx_sort_files_flags(t_files *files, int flags) {
         return;
     }
     mx_sort_files(files, mx_std_cmp);
+    files = reverse_files(files, flags);
+}
+
+t_files *reverse_files(t_files *files, int flags) {
+    t_files *previous = NULL;
+    t_files *current = files;
+    t_files *next;
+
+    if (MX_F_ISRL(flags)) {
+        mx_printstr("OK\n");
+        while (current != NULL) {
+            next = current->next;
+            current->next = previous;
+            previous = current;
+            current = next;
+        }
+        files = previous;
+    }
+    return files;
 }
